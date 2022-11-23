7f891b2156e83ef934b87a1c28bd2f809fe0adb8
Iwona Behnke <behnke.iwona@gmail.com>
2022-11-23 10:11:32 +0100

    Partials

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+// Partials
+hbs.registerPartials(__dirname + "/views/partials");
+
 const capitalize = require("./utils/capitalize");
 const projectName = "Let's Play";
 
@@ -32,5 +35,7 @@ app.use("/", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
 
 module.exports = app;
