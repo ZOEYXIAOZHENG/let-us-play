4cbc2bf21f29e0937a01aa05a66cd03165c3dd80
Iwona Behnke <behnke.iwona@gmail.com>
2022-11-24 17:54:09 +0100

    Changes in profile

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,12 @@
 const games = document.querySelectorAll(".profile-details")
 games.forEach((game)=> {
   game.addEventListener('mouseover', () => {
+    console.log("from listener")
    const removeIcon = game.querySelector(".remove").classList.remove("hidden")
   })
   game.addEventListener('mouseout', () => {
     const removeIcon = game.querySelector(".remove").classList.add("hidden")
    })
 })
+
+
